abfacd512b5d5d82970853dc11cc607976a2f44f
pong dev beginer <patiphonpracticwork001@gmail.com>
2023-04-17 14:40:13 +0700

    update name core

--- Database.php
+++ Database.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\core;
+namespace Patiphon\PhpCoreMvc;
 
 use \PDO;
 
--- Rounter.php
+++ Rounter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\core;
+namespace Patiphon\PhpCoreMvc;
 
 use app\controllers\SiteController;
 
